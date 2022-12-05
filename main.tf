db6af3ca9dbde5bca6de908df3ed008bd5db616b
Vishnu Gaur <44326343+vsmac@users.noreply.github.com>
2022-12-05 19:28:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "terraformgithubexample"
+    key                  = "terraformgithubexample.tfstate"
+  }
+}
+
 resource "random_pet" "rg_name" {
   prefix = var.resource_group_name_prefix
 }
